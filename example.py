ebb36adb1b498df6a83bcdf845a2ebe2c4865d2c
Brieuc LHELIAS <blhelias@bi-consulting.local>
2019-01-14 17:22:46 +0100

    DEV - check pooling backpropagation

--- example.py
+++ example.py
@@ -14,49 +14,57 @@ if __name__ == "__main__":
     print("[INPUT] input size: {}".format(img_array.shape))
     ################
 
-    VGG16 = ConvNet([
-            Convolution(64, (3, 3, 3)),
-            ReLU(),
-            Convolution(64, (3, 3, 64)),
-            ReLU(),
-            Pooling(),
-            Convolution(128, (3, 3, 64)),
-            ReLU(),
-            Convolution(128, (3, 3, 128)),
-            ReLU(),
-            Pooling(),
-            Convolution(256, (3, 3, 128)),
-            ReLU(),
-            Convolution(256, (3, 3, 256)),
-            ReLU(),
-            Convolution(256, (3, 3, 256)),
-            ReLU(),
-            Pooling(),
-            Convolution(512, (3, 3, 256)),
-            ReLU(),
-            Convolution(512, (3, 3, 512)),
-            ReLU(),
-            Convolution(512, (3, 3, 512)),
-            ReLU(),
-            Pooling(),
-            Convolution(512, (3, 3, 512)),
-            ReLU(),
-            Convolution(512, (3, 3, 512)),
-            ReLU(),
-            Convolution(512, (3, 3, 512)),
-            ReLU(),
-            Pooling(),
-            # FC,
-            # FC,
-            # FC
+    model = ConvNet([
+        Convolution(2, (3, 3, 3)),
+        ReLU(),
+        Pooling()
         ])
 
-    VGG16.forward(img_array)
-    # Train example
+    model.forward(img_array)
+
+    #VGG16 = ConvNet([
+    #        Convolution(64, (3, 3, 3)),
+    #        ReLU(),
+    #        Convolution(64, (3, 3, 64)),
+    #        ReLU(),
+    #        Pooling(),
+    #        Convolution(128, (3, 3, 64)),
+    #        ReLU(),
+    #        Convolution(128, (3, 3, 128)),
+    #        ReLU(),
+    #        Pooling(),
+    #        Convolution(256, (3, 3, 128)),
+    #        ReLU(),
+    #        Convolution(256, (3, 3, 256)),
+    #        ReLU(),
+    #        Convolution(256, (3, 3, 256)),
+    #        ReLU(),
+    #        Pooling(),
+    #        Convolution(512, (3, 3, 256)),
+    #        ReLU(),
+    #        Convolution(512, (3, 3, 512)),
+    #        ReLU(),
+    #        Convolution(512, (3, 3, 512)),
+    #        ReLU(),
+    #        Pooling(),
+    #        Convolution(512, (3, 3, 512)),
+    #        ReLU(),
+    #        Convolution(512, (3, 3, 512)),
+    #        ReLU(),
+    #        Convolution(512, (3, 3, 512)),
+    #        ReLU(),
+    #        Pooling(),
+    #        # FC,
+    #        # FC,
+    #        # FC
+    #    ])
+
+    #VGG16.forward(img_array)
+    ## Train example
 
     #for epoch in range n_epochs:
     #    pred = VGG16.forward(img_array) # replace img_array with batch of images
-    #    epoch_loss = Loss() # MSE, RMSE, CROSS_entriopy ....
+    #    epoch_loss = Loss() # MSE, RMSE, CROSS_entropy ....
     #    grad = loss.grad(predicted, batch.targets)
     #    VGG16.backward(grad)
 
